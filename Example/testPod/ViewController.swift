3c683a4829936f61f72ca90572e15d8c61835b74
sonjinuk <supersons@naver.com>
2021-12-07 15:03:22 +0900

    Initial commit

---
+++ Example/testPod/ViewController.swift
@@ -0,0 +1,24 @@
+//
+//  ViewController.swift
+//  testPod
+//
+//  Created by sonjinuk on 12/07/2021.
+//  Copyright (c) 2021 sonjinuk. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+}
+
